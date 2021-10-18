17b869802bb130a3c3de6039327e1a8dc2f50e09
Alexander Thaller <alexander.thaller@trivago.com>
2021-10-18 15:59:40 +0200

    Add binding arg and logging

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,34 @@
+use thiserror::Error;
+
 mod audit;
 mod metrics;
 mod parser;
 
 use metrics::Metrics;
 
-fn main() {
-    let binding = "127.0.0.1:9185".parse().unwrap();
-    let exporter = prometheus_exporter::start(binding).unwrap();
+#[derive(Error, Debug)]
+enum Error {
+    #[error("can not parse binding from args: {0}")]
+    ParseSocketAddr(std::net::AddrParseError),
+
+    #[error("can not start exporter")]
+    ExporterStart(prometheus_exporter::Error),
+}
+
+fn main() -> Result<(), Error> {
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "info");
+    }
+    pretty_env_logger::init();
+
+    let binding = std::env::args()
+        .nth(1)
+        .unwrap_or_else(|| "127.0.0.1:8683".to_string())
+        .parse::<std::net::SocketAddr>()
+        .map_err(Error::ParseSocketAddr)?;
+
+    let exporter = prometheus_exporter::start(binding).map_err(Error::ExporterStart)?;
+
     let metrics = Metrics::new();
 
     loop {
